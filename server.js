0ef5f29631c29a9d75d992dfc19d42020ef64e59
NuntichaThummee <nunticha.ppalaloy909@gmail.com>
2025-09-27 20:03:32 -0400

    Create routes for each feature

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const utils = require("./utils");
-
 const app = express();
+
+//------User account--------//
+
+//create user account
+app.post("/user/signUp", (req, res, next) => {
+
+});
+
+//update user information
+app.put("/user/informationUpdate", (req, res, next) => {
+
+});
+
+//sign in into user account
+app.get("/user/signIn", (req, res, next) => {
+
+});
+
+
+//------Search recipes--------//
 
 //test get first 20 recipes
 app.get("/recipes", async (req, res, next) => {
@@ -12,6 +31,51 @@ app.get("/recipes", async (req, res, next) => {
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
+});
+
+//get recipes by search - user search in input field 
+app.get("/recipes/search", (req, res, next) => {
+
+});
+
+//get recipes by filter 
+app.get("/recipes/filter", (req, res, next) => {
+
+});
+
+//------Add into user saved page--------//
+
+//add recipe choise into user saved pgae 
+app.post("/bookMark/save", (req, res, next) => {
+
+});
+
+//remove recipe choise out of user saved pgae
+app.delete("/bookMark/save", (req, res, next) => {
+
+});
+
+//------Create shopping list--------//
+
+//create shopping list from recipe choise and add into shopping list page
+app.post("/shoppingList/add", (req, res, next) => {
+
+});
+
+//remove shopping list from recipe choise out of the shopping list page
+app.delete("/shoppingList/remove", (req, res, next) => {
+
+});
+
+//------Random recipes menu--------//
+
+//get recipes by random index
+app.get("/random", (req, res, next) => {
+
+});
+
+//get recipes by random index with filter
+app.get("/random/filter", (req, res, next) => {
 
 });
 
